e3cfd0762806eca307f7802c0e4a1d5a4ba22ae2
Akingbola1234 <akingbolaola@gmail.com>
2021-11-02 16:04:31 +0100

    Added all functionalities need for the Script.js will be added the random feature later

--- script.js
+++ script.js
@@ -47,6 +47,9 @@ let playSong = () => {
     musicContainer.classList.add("play");
     playBtn.querySelector("i.fas").classList.remove("fa-play");
     playBtn.querySelector("i.fas").classList.add("fa-pause");
+
+    //Let's play the song with the audio tag API .play
+    audio.play();
 };
 
 //Creating the pauseSong Function
@@ -55,6 +58,9 @@ let pauseSong = () => {
     musicContainer.classList.remove("play");
     playBtn.querySelector("i.fas").classList.remove("fa-pause");
     playBtn.querySelector("i.fas").classList.add("fa-play");
+
+    //Let's pause the song with the audio tag API .pause
+    audio.pause();
 };
 
 //Creating the Pause and Play Toggle
@@ -67,6 +73,68 @@ playBtn.addEventListener("click", () => {
         playSong();
     }
 });
+
+//Proceeding to the previous and Next button
+
+//Previous Button
+let prevSong = () => {
+    //Decreasing the songIndex by one to get to the previous song
+    songIndex--;
+    //Checking if the songIndex is less than 0
+    if (songIndex < 0) {
+        //Changing the songIndex to start from begining once the songIndex is less than 0
+        songIndex = songs.length - 1;
+    }
+    //Then Loaded the song back with the current songIndex
+    loadSong(songs[songIndex]);
+    //Playing the Song
+    playSong();
+};
+
+prevBtn.addEventListener("click", prevSong);
+
+let nextSong = () => {
+    songIndex++;
+    if (songIndex > songs.length - 1) {
+        songIndex = 0;
+    }
+    loadSong(songs[songIndex]);
+    playSong();
+};
+nextBtn.addEventListener("click", nextSong);
+
+//Now ProgressBar of the audio, Because up till now the Progress Bar is not working yet
+
+//updateProgress will take in an event object on this event object we can get the duration of the song, and the currentime
+let updateProgress = (e) => {
+    //From the srcElement let's deconstruct the duration and the currentTime
+    const { duration, currentTime } = e.srcElement;
+    const progressPercent = (currentTime / duration) * 100;
+
+    //Thinking of Getting the Total Duration too outside
+    //Then Let's set the width of the Progressbar to whatever the progressPercent is
+    progress.style.width = `${progressPercent}%`;
+};
+//Time update is an event that comes with the Html5 audio tag which is constantly run evertime an audio is playing and here on time update we want it to listen to a function updateProgress
+audio.addEventListener("timeupdate", updateProgress);
+
+//Now let's add being able to click on any area in the progressContainer to fastforward the song or backward the song
+
+let setProgress = (e) => {
+    //This will get the width of progressContainer
+    const width = progressContainer.clientWidth;
+    const clickX = e.offsetX;
+    const duration = audio.duration;
+    audio.currentTime = (clickX / width) * duration;
+};
+progressContainer.addEventListener("click", setProgress);
+
+//The Last thing we want it to be when the song ends it should go to the next song
+//The audio API gives us an event called "ended" i.e what happens when the song ends
+//Since we already have a nextSong function created which is to play the next song we can easily add that
+audio.addEventListener("ended", nextSong);
+
+//Doing Some Testings with Toggle Learnt
 
 let inputDiv = document.querySelector("#inputdiv");
 let input = document.querySelector("#input");
